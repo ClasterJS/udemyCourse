ac1ff80079111f2df9cf84d1f9291ab1e848cbeb
ClasterJS <Claster85@gmail.com>
2020-11-29 12:32:39 +0300

    lesson 15

--- script.js
+++ script.js
@@ -1,18 +1,39 @@
-const numberOfFilms = +prompt('Сколько фильмов Вы уже посмотрели?',''),
-      personalMovieDB = {
-        count: numberOfFilms,
-        movies: {},
-        actors: {},
-        genres: [],
-        privat: false
-};
+"use strict";
+const numberOfFilms = +prompt("Сколько фильмов Вы уже посмотрели?", ""),
+  personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+  };
 
-let lastFilm = prompt('Один из последних просмотренных фильмов?', ''),
-    ratingFilm = prompt('На сколько оцените его?', ''),
-    lastFilm2 = prompt('Один из последних просмотренных фильмов?', ''),
-    ratingFilm2 = prompt('На сколько оцените его?', '');
+for (let i = 1; i <= 2; i++) {
+  let lastFilm = prompt("Один из последних просмотренных фильмов?", ""),
+    ratingFilm = prompt("На сколько оцените его?", "");
+  if (
+    lastFilm.length == 0 ||
+    lastFilm.length > 50 ||
+    lastFilm == null ||
+    ratingFilm == null ||
+    ratingFilm.length == 0
+  ) {
+    console.log("Error");
+    i--;
+  } else {
+    console.log("Done");
+    personalMovieDB.movies[lastFilm] = ratingFilm;
+  }
+}
 
-personalMovieDB.movies[lastFilm] = ratingFilm;
-personalMovieDB.movies[lastFilm2] = ratingFilm2;
+if (personalMovieDB.count < 10) {
+  console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+  console.log("Вы киноман");
+} else {
+  console.log("Произошла ошибка");
+}
 
 console.log(personalMovieDB);
